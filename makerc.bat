57c43656f59638b974e34bf83f93e8b77640b64f
あると <mail@twinkfrag.net>
2015-03-06 20:38:33 +0900

    add .gitconfig

--- makerc.bat
+++ makerc.bat
@@ -2,10 +2,11 @@ echo off
 openfiles > NUL 2>&1 
 if NOT %ERRORLEVEL% EQU 0 goto NotAdmin 
 echo ä«óùé“å†å¿Ç≈é¿çsíÜ
+mkdir %localappdata%\vim
 mklink %userprofile%\_vimrc %~dp0.vimrc
 mklink %userprofile%\_gvimrc %~dp0.gvimrc
 mklink %userprofile%\.gitignore %~dp0.gitignore
-mklink /d %userprofile%\.git %~dp0.git
+mklink %userprofile%\.gitconfig %~dp0.gitconfig
 pause
 goto End
  
