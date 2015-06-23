8c384c1d201f33d2b7c1032e553b3c9439aa7c49
あると <mail@twinkfrag.net>
2015-06-23 18:40:34 +0900

    add PowerShell PoshGit function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,10 @@
 $window = (Get-Host).UI.RawUI
 $window.WindowTitle = "Script Center"
 
+# if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
+# 
+# # Load posh-git example profile
+# . 'C:\tools\poshgit\dahlbyk-posh-git-bc8dbd5\profile.example.ps1'
+# 
+# Rename-Item Function:\Prompt PoshGitPrompt -Force
+# function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
